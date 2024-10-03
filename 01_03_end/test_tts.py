bb37327116b8ddaa1daa1ad1eb57a8e3d5c18033
David Martins <xakal_1@msn.com>
2024-10-03 22:35:16 +0100

    Update test_tts.py
    Fix issue on file not running due to python library issue.

--- 01_03_end/test_tts.py
+++ 01_03_end/test_tts.py
@@ -3,10 +3,9 @@ from openai import OpenAI
 client = OpenAI()
 
 speech_file_path = Path(__file__).parent / "speech.mp3"
-response = client.audio.speech.create(
+with client.audio.speech.with_streaming_response.create(
   model="tts-1",
   voice="echo",
   input="Today is a wonderful day to build something people love!"
-)
-
+) as response:
 response.stream_to_file(speech_file_path)
